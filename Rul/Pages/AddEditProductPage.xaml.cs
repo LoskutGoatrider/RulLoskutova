4574c4a2740b3315be988447176f8cf9ee20aa45
LoskutGoatrider <rayok8951@gmail.com>
2025-04-08 15:22:17 +0700

    Добавьте файлы проекта.

---
+++ Rul/Pages/AddEditProductPage.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using Rul.Entities;
+
+namespace Rul.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditProductPage.xaml
+    /// </summary>
+    public partial class AddEditProductPage : Page
+    {
+        Product product = new Product();
+
+        public AddEditProductPage(Product currentProduct)
+        {
+            InitializeComponent();
+
+            if (currentProduct != null) 
+            {
+                product = currentProduct;
+
+                btnDeleteProduct.Visibility = Visibility.Visible;
+                txtArticle.IsEnabled = false;
+            }
+            DataContext = product;
+            cmbCategory.ItemsSource = CategoryList;
+        }
+
+        public string[] CategoryList =
+        {
+            "Аксессуары",
+            "Автозапчасти",
+            "Автосервис",
+            "Съемники подшипников",
+            "Ручные инструменты",
+            "Зарядные устройства"
+        };
+
+        private void btnEnterImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog getImageDialog = new OpenFileDialog();
+
+            getImageDialog.Filter = "Файды изображений: (*.png, *.jpg, *.jpeg)| *.png; *.jpg; *.jpeg";
+            getImageDialog.InitialDirectory = "D:\\programmesmodules\\Rul\\Rul\\Resources";
+            if (getImageDialog.ShowDialog() == true)
+            {
+                product.ProductImage = getImageDialog.SafeFileName;
+                img.Source = new BitmapImage(new Uri(getImageDialog.FileName));
+            }
+        }
+
+        private void btnDeleteProduct_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show($"Вы действительно хотите удалить {product.ProductName}?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    TradeEntities.GetContext().Product.Remove(product);
+                    TradeEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Запись удалена!", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+                    NavigationService.GoBack();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void btnSaveProduct_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            TradeEntities db = new TradeEntities();
+
+            if (product.ProductCost < 0)
+                errors.AppendLine("Стоимость не может быть отрицательной!");
+            if (product.MinCount < 0)
+                errors.AppendLine("Минимальное количество не может быть отрицательным!");
+            if (product.ProductDiscountAmount > product.MaxDiscountAmount)
+                errors.AppendLine("Действующая скидка на товар не может быть больше максимальной скидки!");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if (!db.Product.Any(x => x.ProductArticleNumber == txtArticle.Text))
+            {
+                try
+                {
+                    db.Product.Add(product);
+                    db.SaveChanges();
+                    MessageBox.Show("Информация сохранена!", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+                    NavigationService.GoBack();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                try
+                {
+                    var product = db.Product.FirstOrDefault(x => x.ProductArticleNumber == txtArticle.Text);
+                    product.ProductManufacturer = txtManufacturer.Text;
+                    product.ProductCategory = cmbCategory.Text;
+                    product.ProductQuantityInStock = Convert.ToInt32(txtCountInStock.Text);
+                    product.Unit = txtUnit.Text;
+                    product.ProductDiscountAmount = Convert.ToByte(txtDiscount.Text);
+                    product.Supplier = txtSupplier.Text;
+                    product.MaxDiscountAmount = Convert.ToByte(txtMaxDiscount.Text);
+                    db.SaveChanges();
+                    MessageBox.Show("Информация сохранена!", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
---
+++ Rul/Pages/Autho.xaml.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Rul.Entities;
+using Rul.Services;
+
+namespace Rul.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Autho.xaml
+    /// </summary>
+    public partial class Autho : Page
+    {
+        private DispatcherTimer timer;
+        private int remainingTime;
+        int click;
+
+        public Autho()
+        {
+            InitializeComponent();
+            CreateTimer();
+            click = 0;
+        }
+
+        private void CreateTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+        }
+
+        private void btnEnterGuests_Click(object sender, RoutedEventArgs e)
+        {
+            LoadPage(null, null);
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            click += 1;
+            string login = txtbLogin.Text.Trim();
+            string password = pswbPassword.Password.Trim();
+
+            var context = TradeEntities.GetContext();
+
+            var user = context.User.Where(x => x.UserLogin == login && x.UserPassword == password).FirstOrDefault();
+
+            if (click == 1)
+            {
+                if (user != null)
+                {
+                    txtbLogin.Clear();
+                    pswbPassword.Clear();
+                    LoadPage(user, user.UserRole.ToString());
+                }
+                else
+                {
+                    MessageBox.Show("Вы ввели логин или пароль неверно!");
+                    GenerateCapctcha();
+
+                    pswbPassword.Clear();
+
+                    tblCaptcha.Visibility = Visibility.Visible;
+                    tblCaptcha.Text = CaptchaGenerator.GenerateCaptchaText(6);
+                }
+            }
+            else if (click > 1)
+            {
+                if (click == 3)
+                {
+                    BlockControls();
+
+                    remainingTime = 10;
+                    txtbTimer.Visibility = Visibility.Visible;
+                    timer.Start();
+                }
+
+                if (user != null && tbCaptcha.Text == tblCaptcha.Text)
+                {
+                    txtbLogin.Clear();
+                    pswbPassword.Clear();
+                    tblCaptcha.Text = "Text";
+                    tbCaptcha.Text = "";
+                    tbCaptcha.Visibility = Visibility.Hidden;
+                    tblCaptcha.Visibility = Visibility.Hidden;
+                    LoadPage(user, user.UserRole.ToString());
+                }
+                else
+                {
+
+                    tblCaptcha.Text = CaptchaGenerator.GenerateCaptchaText(6);
+                    tbCaptcha.Text = "";
+                    MessageBox.Show("Пройдите капчу заново!");
+                }
+            }
+        }
+
+        private void LoadPage(User user, string role)
+        {
+            click = 0;
+            switch (role)
+            {
+                case "1":
+                    NavigationService.Navigate(new Admin(user));
+                    break;
+                case "2":
+                    NavigationService.Navigate(new Client(user));
+                    break;
+                case "3":
+                    NavigationService.Navigate(new Client(user));
+                    break;
+                default:
+                    NavigationService.Navigate(new Client(user));
+                    break;
+            }
+        }
+
+        private void GenerateCapctcha()
+        {
+            tbCaptcha.Visibility = Visibility.Visible;
+            tblCaptcha.Visibility = Visibility.Visible;
+
+            string capctchaText = CaptchaGenerator.GenerateCaptchaText(6);
+            tblCaptcha.Text = capctchaText;
+            tblCaptcha.TextDecorations = TextDecorations.Strikethrough;
+        }
+
+        private void BlockControls()
+        {
+            txtbLogin.IsEnabled = false;
+            pswbPassword.IsEnabled = false;
+            tbCaptcha.IsEnabled = false;
+            btnEnterGuests.IsEnabled = false;
+            btnEnter.IsEnabled = false;
+        }
+
+        private void UnlockControls()
+        {
+            txtbLogin.IsEnabled = true;
+            pswbPassword.IsEnabled = true;
+            tbCaptcha.IsEnabled = true;
+            btnEnterGuests.IsEnabled = true;
+            btnEnter.IsEnabled = true;
+            txtbLogin.Clear();
+            pswbPassword.Clear();
+            tblCaptcha.Text = "Text";
+            tbCaptcha.Text = "";
+            tbCaptcha.Visibility = Visibility.Hidden;
+            tblCaptcha.Visibility = Visibility.Hidden;
+            click = 0;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            remainingTime--;
+
+            if (remainingTime <= 0)
+            {
+                timer.Stop();
+                UnlockControls();
+                txtbTimer.Visibility = Visibility.Hidden;
+                return;
+            }
+
+            txtbTimer.Text = $"Оставшееся время: {remainingTime} секунд";
+        }
+    }
+}
